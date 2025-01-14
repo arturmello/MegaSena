9edfd8ee25c85d0113234bccd107e790e68b6855
Artur Mello <arturmellog19@gmail.com>
2025-01-13 23:57:59 -0300

    Refatorando Refactoring input validation logic in the MegaSena app

--- app/src/main/java/com/example/megasena/MainActivity.kt
+++ app/src/main/java/com/example/megasena/MainActivity.kt
@@ -1,7 +1,6 @@
 package com.example.megasena
 
 import android.os.Bundle
-import android.util.Log
 import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
@@ -27,36 +26,36 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
+
     private fun numberGenerator(text: String, txtResult: TextView) {
-        //Validar quando o campo é vazio
-        if(text.isNotEmpty()) {
 
-            // Convertendo a String em Inteiro
-            val qtd = text.toInt()
-
-            //Validar se o usuário digitou um número entre 6 e 15
-            if (qtd >= 6 && qtd <= 15) {
-
-                val numbers = mutableSetOf<Int>()
-                val random = Random()
-
-                while(true) {
-                    val number = random.nextInt(60) // Irá gerar números de 0 a 59
-                    numbers.add(number + 1)
-
-                    if (numbers.size == qtd) {
-                        break
-                    }
-                }
-
-                txtResult.text = numbers.joinToString(" - ")
-
-            } else {
-                Toast.makeText(this, "Informe um número entre 6 e 15", Toast.LENGTH_LONG).show()
-            }
-
-        } else {
+        //Lógica de falha para caso o campo de texto, esteja vazio
+        if (text.isEmpty()) {
             Toast.makeText(this, "Informe um número entre 6 e 15", Toast.LENGTH_LONG).show()
+            return
         }
+
+        //Lógica de falha para caso o número digitado não esteja entre 6 e 15
+        val qtd = text.toInt() //Converte String para Inteiro
+
+        if (qtd < 6 || qtd > 15) {
+            Toast.makeText(this, "Informe um número entre 6 e 15", Toast.LENGTH_LONG).show()
+            return
+        }
+
+        //Lógica de funcionamento de sucesso do app
+        val numbers = mutableSetOf<Int>()
+        val random = Random()
+
+        while (true) {
+            val number = random.nextInt(60) // Irá gerar números de 0 a 59
+            numbers.add(number + 1)
+
+            if (numbers.size == qtd) {
+                break
+            }
+        }
+
+        txtResult.text = numbers.joinToString(" - ")
     }
 }
