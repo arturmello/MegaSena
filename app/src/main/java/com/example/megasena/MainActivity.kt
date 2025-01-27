ae3a9f89b717f95f3b3e83d81654c6f9da2fbf78
Artur Mello <arturmellog19@gmail.com>
2025-01-27 06:05:42 -0300

    Added new field that saves the last game made by the user, using SharedPreferences

--- app/src/main/java/com/example/megasena/MainActivity.kt
+++ app/src/main/java/com/example/megasena/MainActivity.kt
@@ -1,5 +1,7 @@
 package com.example.megasena
 
+import android.content.Context
+import android.content.SharedPreferences
 import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
@@ -9,14 +11,27 @@ import androidx.appcompat.app.AppCompatActivity
 import java.util.Random
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var prefs: SharedPreferences
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
 
+
         val editText = findViewById<EditText>(R.id.edit_number)
         val txtResult = findViewById<TextView>(R.id.txt_number)
+        val txtRegister = findViewById<TextView>(R.id.txt_register)
         val btnGenerate = findViewById<Button>(R.id.btn_generate)
+
+        //Preferências
+        prefs = getSharedPreferences("db", Context.MODE_PRIVATE)
+        val lastResult = prefs.getString("result", null)
+
+        if (lastResult != null) {
+            txtRegister.text = "Última aposta: $lastResult"
+        }
 
         btnGenerate.setOnClickListener {
 
@@ -57,5 +72,10 @@ class MainActivity : AppCompatActivity() {
         }
 
         txtResult.text = numbers.joinToString(" - ")
+
+        val editor = prefs.edit()
+        editor.putString("result", txtResult.text.toString())
+        editor.apply()
+
     }
 }
